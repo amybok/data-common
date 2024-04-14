a121117d01152afb8adcac5ee38a4ff28da1303e
skyeha <nhatanhhado@gmail.com>
2024-04-14 14:47:46 +1000

    mothing nuch

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
-import Login from "../components/Login.jsx"
-import Head from "../components/Head.jsx"
-import illustration from "../assets/illustration.png"
-import LoginButton from "../components/LogInButton.jsx"
+import Login from "../components/Login.jsx";
+import Head from "../components/Head.jsx";
+import illustration from "../assets/illustration.png";
+import LoginButton from "../components/LogInButton.jsx";
 
 const LoginPage = () => {
   const style = {
@@ -36,20 +36,39 @@ const LoginPage = () => {
     },
   };
 
-    return (
-        <div style={{backgroundImage:`url(${illustration})`, minHeight: "91vh", height:"auto"}}>
-            <div style={{paddingTop:"120px"}}>
-                <h1 style={style.h1}> Login to the Data Commons</h1>
-                <h3 style={style.h4}> Sign in to continue </h3>
-                <div style={{display:"flex", alignSelf:"center", justifyContent:"center"}}>
-                    <Login/>
-                </div>
-                <div style={{display:"flex", alignSelf:"center", justifyContent:"center", paddingTop:"30px"}}>
-                    <LoginButton/>
-                </div>
-            </div>
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${illustration})`,
+        minHeight: "91vh",
+        height: "auto",
+      }}
+    >
+      <div style={{ paddingTop: "120px" }}>
+        <h1 style={style.h1}> Login to the Data Commons</h1>
+        <h3 style={style.h4}> Sign in to continue </h3>
+        <div
+          style={{
+            display: "flex",
+            alignSelf: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Login />
         </div>
-    )
-}
+        <div
+          style={{
+            display: "flex",
+            alignSelf: "center",
+            justifyContent: "center",
+            paddingTop: "30px",
+          }}
+        >
+          <LoginButton />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
