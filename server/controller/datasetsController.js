82433336661dabe4331de7ac9fc909ace6d3f2a1
Amy <amy.gh.ly@gmail.com>
2024-04-17 22:55:39 +1000

    add post method to datasets api controller

--- server/controller/datasetsController.js
+++ server/controller/datasetsController.js
@@ -5,26 +5,52 @@ const datasetsApi = express.Router();
 
 // Create a route and a handler for GET /datasets
 datasetsApi.get('/', (req, res) => {
-    // Send the posts array as a JSON response
+    // Send the datasets array as a JSON response
     res.status(200).json(datasets);
 });
 
-// Create a route and a handler for GET /posts/:id
+// Create a route and a handler for GET /datasets/:id
 datasetsApi.get('/:id', (req, res) => {
     // Get the id parameter from the request
     const rid = req.params.id;
     
 
-    // Find the post with the given id in the posts array
+    // Find the dataset with the given id in the datasets array
     const dataset = datasets.find((d) => d.id == rid);
     console.log(dataset)
 
-    // If the post exists, send it as a JSON response
+    // If the dataset exists, send it as a JSON response
     if (dataset) {
         res.json(dataset);
     } else {
-        // If the post does not exist, send a 404 status code and a message
+        // If the dataset does not exist, send a 404 status code and a message
         res.status(404).send('Dataset not found');
+    }
+});
+
+// Create a route and a handler for POST /datasets
+datasetsApi.post((req, res) => {
+    // To handle the request body, we need to use a middleware called express.json
+    // This middleware parses the request body as JSON and adds it to the req object
+    app.use(express.json());
+    
+    // Get the data from the request body
+    const data = req.body;
+
+    // Validate the data
+    if (data.name && data.method) {
+        const newId = "PDC00" + datasets.length + 1;
+        const newDataset = new Dataset(newId, data.name, data.method);
+
+        // Add the new dataset to the datasets array
+        datasets.push(newDataset);
+
+        // Send a 201 status code and the new dataset as a JSON response
+        res.status(201).json(newDataset);
+    }
+    else {
+        // If the data is invalid, send a 400 status code and a message
+        res.status(400).send('Invalid data');
     }
 });
 
