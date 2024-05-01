6fc771413fc367c6957badc2c85889a894d812bc
Ubuntu <ubuntu@amy.novalocal>
2024-05-01 13:34:23 +0000

    modified port for deploy

--- server/index.js
+++ server/index.js
@@ -17,4 +17,4 @@ app.get("/", (req, res) => {
     res.send("Successful response")
 });
 
-app.listen(3000, () => {console.log("Server started on port 3000")})
+app.listen(3001, () => {console.log("Server started on port 3001")})
