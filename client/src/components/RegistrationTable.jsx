baaf8a2b5722b871d8452b24c8a0f7716ff6cdf5
Amy <amy.gh.ly@gmail.com>
2024-05-04 17:11:22 +1000

    registration takes inputs in as state object

--- client/src/components/RegistrationTable.jsx
+++ client/src/components/RegistrationTable.jsx
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 
 const RegistrationTable = () => {
     
+    const [formData, setFormData] = useState({
+        name:'',
+        description:'',
+        method:'',
+        file_path:'',
+        portal_link:''
+    })
 
     const style = {
         form: {
@@ -16,10 +23,11 @@ const RegistrationTable = () => {
         table: {
             display:"flex", 
             justifyContent:"center",
-            borderRadius: "8px",
-            border: "1px solid",
-            padding: "20px",
-            marginBottom: "10px"
+            borderRadius: "5px",
+            padding: "10px",
+            marginBottom: "10px",
+            backgroundColor: "white",
+            boxShadow: "0px 5px 10px 0px rgba(0,0,0,0.5)",
         },
         button: {
             backgroundColor: "darkslategrey",
@@ -30,14 +38,27 @@ const RegistrationTable = () => {
         }
     }
 
+    const handleInputChange = (event) => {
+        
+        const { name, value} = event.target;
 
-    const handleSubmit = (event) => {
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]:value
+        }));
+
+        console.log(formData)
+    }
+
+    /* const handleSubmit = (event) => {
         event.preventDefault();
 
         const formData = new FormData(event.target);
         const inputValue = formData.get('inputName');
 
         sendInputValueToApi(inputValue).then(() => 
+
+            // (`http://115.146.86.176/api/datasets`) -- production url
             fetch("http://localhost:3000/api/datasets", {
                 method: "POST",
                 body: JSON.stringify({"name": this.state.name, "method": this.state.method}),
@@ -45,10 +66,12 @@ const RegistrationTable = () => {
             })
         )  
     };
+    */
     
     return (
         <div style={{display:"flex", justifyContent:"space-around"}}>
-            <form onSubmit={handleSubmit} style={style.form}>
+            {/*<form onSubmit={handleSubmit} style={style.form}>*/}
+            <form style={style.form}>
 
                 <h1 style={{fontFamily:"Helvetica", fontWeight:"Lighter", marginTop:"50px", marginBottom:"20px"}}>
                     Register new dataset
@@ -58,27 +81,31 @@ const RegistrationTable = () => {
                     <tbody>
                         <tr>
                             <th style={style.label}>Cell type</th>
-                            <button style={style.button}>Cell type</button>
+                            <td>
+                                <button style={style.button}>Cell type</button>
+                            </td>
                         </tr>
                         <tr>
                             <th style={style.label}><label>Name</label></th>
-                            <td><input type="text"/></td>
+                            <td><input type="text" name="name" value={formData.name} onChange={(e) => handleInputChange(e)}/></td>
                         </tr>
                         <tr>
                             <th style={style.label}><label>Description</label></th>
-                            <td><input type="text"/></td>
+                            <td><input type="text" name="description" value={formData.description} onChange={(e) => handleInputChange(e)}/></td>
                         </tr>
                         <tr>    
                             <th style={style.label}><label>Method</label></th>
-                            <td><button style={style.button}>Method</button></td>
+                            <td>
+                                <button style={style.button}>Method</button>
+                            </td>
                         </tr>
                         <tr>
                             <th style={style.label}><label>File path</label></th>
-                            <td><input type="text"/></td>
+                            <td><input type="text" name="file_path" value={formData.file_path} onChange={(e) => handleInputChange(e)}/></td>
                         </tr>
                         <tr>
                             <th style={style.label}><label>Link to portals</label></th>
-                            <td><input type="text"/></td>
+                            <td><input type="text" name="portal_link" value={formData.portal_link} onChange={(e) => handleInputChange(e)}/></td>
                         </tr>
                     </tbody>
                 </table>
