b6835aa631b2308644954d9f1f2d20745461d5a3
skyeha <nhatanhhado@gmail.com>
2024-05-12 18:00:02 +1000

    nothing much

--- client/src/components/DataInfo.jsx
+++ client/src/components/DataInfo.jsx
@@ -29,8 +29,11 @@ const DataInfo = ({ data, main_id }) => {
 
   const getCode = async (scriptName) => {
     try {
+      // const response = await fetch(
+      //   `http://localhost:3001/api/code/${scriptName}`
+      // );
       const response = await fetch(
-        `http://localhost:3001/api/code/${scriptName}`
+        `http://115.146.86.176/api/code/${scriptName}`
       );
       const content = await response.text();
 
