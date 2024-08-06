de261b4b008d4b104bd6e134b966eb70708dda66
Amy <amy.gh.ly@gmail.com>
2024-08-06 18:10:16 +1000

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: "/data-common/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 })
